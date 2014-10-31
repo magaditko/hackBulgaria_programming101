012036dbd0ad42c7eed958b34ca1064068732e2c
Margarita Vasileva <m.h.vasileva@gmail.com>
2014-11-01 00:27:35 +0200

    finish Dungeons and Pythons task

--- week2/Dungeons-and-Pythons/dungeon.py
+++ week2/Dungeons-and-Pythons/dungeon.py
@@ -4,13 +4,14 @@ from random import choice
 from hero import Hero
 from orc import Orc
 from weapon import Weapon
+from fight import Fight
 
 
 class Dungeon:
 
     def __init__(self, path):
         self.path = path
-        self.dungeon = []
+        self.d_map = {}
         self.weapons = {}
         self.set_dungeon()
         self.players = {}
@@ -18,76 +19,156 @@ class Dungeon:
     def set_dungeon(self):
         file = open(self.path)
         content = file.read().split('\n')
-        rn = 0
+        row = 0
 
         for row in enumerate(content):
             if row[1] == '':
-                rn = row[0]
+                row = row[0]
                 break
-            self.dungeon.append(list(row[1]))
+            for col in enumerate(row[1]):
+                self.d_map[(row[0], col[0])] = col[1]
 
-        self.set_weapons(rn, content)
+        self.set_weapons(row, content)
         file.close()
 
-    def set_weapons(self, start, content):
+    def set_weapons(self, start_row, content):
         weapon = []
-        for x in range(start, len(content)):
+        for x in range(start_row, len(content)):
             if content[x] != '':
                 weapon.append(content[x].split(' '))
         for w in weapon:
-            self.weapons[w[0]] = [Weapon(w[0], int(w[1]), float(w[2])), []]
-                
-    def print_map(self):
-        for row in self.dungeon:
-            for col in row:
-                print(col, end = '')
-            print()
+            self.weapons[w[0]] = {'details': Weapon(w[0], int(w[1]), float(w[2])), 'coordinates': ()}
 
     def find_spawn(self):
         result = []
-        for row in enumerate(self.dungeon):
-            if 'S' in row[1]:
-                index_spawn = row[1].index('S')
-                result.append((row[0], index_spawn))
-
-        if result == []:
-            return False
-        else:
+        for coord in self.d_map:
+            if self.d_map[coord] == 'S':
+                result = coord
+                break
+        if result:
             return result
+        else:
+            return False
 
-    def get_entity_type(self, entity):
-        if isinstance(entity, Hero):
+    def find_letter(self, instance):
+        if type(instance) == Hero:
             return 'H'
-        elif isinstance(entity, Orc):
+        elif type(instance) == Orc:
             return 'O'
 
-    def spawn(self, player, entity):
-        spawn_points = self.find_spawn()
-        if not spawn_points:
+    def spawn(self, player_name, entity):
+        spawn_coordinates = self.find_spawn()
+        player_letter = self.find_letter(entity)
+        if player_name in self.players or not self.find_spawn():
             return False
         else:
-            x, y = spawn_points[0][0], spawn_points[0][1]
-            self.players[player] = [entity, [x, y]]
-            self.dungeon[x][y] = self.get_entity_type(entity)
+            self.players[player_name] = {'details': entity, 'coordinates': spawn_coordinates}
+            self.d_map[spawn_coordinates] = player_letter
             return True
 
-    def get_next_position_coordinates(self, current, direction):
-        step = 1
-        next_pos = copy.copy(current)
-        if direction == 'left':
-            next_pos[1] -= step
-        elif direction == 'right':
-            next_pos[1] += step
-        elif direction == 'up':
-            next_pos[0] -= step
-        elif direction == 'down':
-            next_pos[0] += step
+    def print_map(self):
+        coordinates = []
+        for c in self.d_map:
+            coordinates.append(c)
+        coordinates.sort()
+        col_count = 1
+        for c in range(len(coordinates) - 1):
+            if coordinates[c][0] == coordinates[c + 1][0]:
+                col_count += 1
+            else:
+                break
+                
+        board = [coordinates[i: i + col_count] for i in range(0, len(coordinates), col_count)]
 
-        if next_pos[0] < 0 or next_pos[0] > len(self.dungeon) or next_pos[1] < 0 or next_pos[1] > len(self.dungeon[0]):
+        for row in board:
+            for col in row:
+                print(self.d_map[col], end='')
+            print()
+
+    def sp_weapons(self):
+        coordinates = []
+        for k in self.d_map:
+            if self.d_map[k] == '.':
+                coordinates.append(k)
+        return coordinates
+
+    def spawn_weapons(self):
+        sp_coordinates = self.sp_weapons()
+        for weapon in self.weapons:
+            coordinates = choice(sp_coordinates)
+            self.weapons[weapon]['coordinates'] = coordinates
+            self.d_map[coordinates] = 'W'
+            sp_coordinates.remove(coordinates)
+
+    def get_next_coordinates(self, current_coordinates, direction):
+        next_coordinates = list(current_coordinates)
+        if direction == 'right':
+            next_coordinates[1] += 1
+        elif direction == 'left':
+            next_coordinates[1] -= 1
+        elif direction == 'up':
+            next_coordinates[0] -= 1
+        elif direction == 'down':
+            next_coordinates[0] += 1
+
+        if tuple(next_coordinates) not in self.d_map:
             return False
         else:
-            return next_pos
+            return tuple(next_coordinates)
 
+    def get_weapon(self, coordinates):
+        for weapon in self.weapons:
+            if self.weapons[weapon]['coordinates'] == coordinates:
+               return self.weapons[weapon]['details']
 
+    def return_enemy(self, next_coordinates):
+        for player in self.players:
+            if self.players[player]['coordinates'] == next_coordinates:
+                return self.players[player]['details']
+
+    def return_self(self, player_name):
+        return self.players[player_name]['details']
+
+    def move(self, player_name, direction):
+        p_current = self.players[player_name]['coordinates']
+        p_letter = self.find_letter(self.players[player_name]['details'])
+        p_next = self.get_next_coordinates(p_current, direction)
         
-    
+        enemy = lambda p_letter: 'O' if p_letter == 'H' else 'H'
+        
+        if not p_next:
+            return False
+        elif self.d_map[p_next] == '#':
+            return False
+        elif self.d_map[p_next] == '.':
+            self.d_map[p_current] = '.'
+            self.d_map[p_next] = p_letter
+            self.players[player_name]['coordinates'] = p_next
+            return True
+        elif self.d_map[p_next] == 'W':
+            self.weapon_case(player_name, p_next, p_current, p_letter)
+            return True
+        elif self.d_map[p_next] == enemy:
+            self.enemy_case(p_next, player_name)
+            
+    def enemy_case(self, p_next, player_name):
+        if type(self.return_self(player_name)) == Hero:
+            second = self.return_self(player_name)
+            first = self.return_enemy(p_next)
+        else:
+            second = self.return_enemy(p_next)
+            first = self.return_self(player_name)
+
+        print('Wild {} appeared!'.format(self.return_enemy(p_next)))
+        print('FIGHT!')
+        fight = Fight(first, second)
+        winner = fight.simulate_fight()
+        print('{} WINS!'.format(winner))
+        return winner
+        
+    def weapon_case(self, player_name, p_next, p_current, p_letter):
+        weapon = self.get_weapon(p_next)
+        self.players[player_name]['details'].equip_weapon(weapon)
+        self.d_map[p_current] = '.'
+        self.d_map[p_next] = p_letter
+        self.players[player_name]['coordinates'] = p_next
--- week2/Dungeons-and-Pythons/tests/dungeon_test.py
+++ week2/Dungeons-and-Pythons/tests/dungeon_test.py
@@ -4,86 +4,161 @@ import copy
 import sys
 sys.path.insert(0, '../')
 
-from dungeon import Dungeon
+from dungeon_new import Dungeon
 from hero import Hero
 from orc import Orc
+from weapon import Weapon
 
 
 class DungeonTest(unittest.TestCase):
 
     def setUp(self):
         self.test_dungeon = Dungeon('dungeon.txt')
-        self.sho_hero = Hero('Shosh', 100, 'GraveRobber')
-        self.shatterer_orc = Orc('Shatterer', 100, 1.0)
+        self.t_hero = Hero('Shosh', 100, 'GraveRobber')
+        self.t_orc = Orc('Shatterer', 100, 1)
+        self.small = Dungeon('d_map_small.txt')
 
     def test_init(self):
         self.assertEqual('dungeon.txt', self.test_dungeon.path)
 
-    def test_find_spawn_points(self):
-        self.assertEqual([(0, 0), (4, 9)], self.test_dungeon.find_spawn())
+    def test_set_weapons(self):
+        result = []
+        for weapon in self.test_dungeon.weapons:
+            result.append(weapon)
+        self.assertIn('BigAxe', result)
+        self.assertEqual(type(self.test_dungeon.weapons['BigAxe']['details']), Weapon)
+        self.assertEqual(self.test_dungeon.weapons['BigAxe']['coordinates'], ())
 
-    def test_get_entity_type(self):
-        self.assertEqual(self.test_dungeon.get_entity_type(self.sho_hero), 'H')
-        self.assertEqual(self.test_dungeon.get_entity_type(self.shatterer_orc), 'O')
+    def test_find_spawn_points(self):
+       
+        self.assertEqual(self.test_dungeon.find_spawn(), (0, 0))
+
+        no_spawn_dungeon = Dungeon('dungeon_no_spawn_points.txt')
+        self.assertFalse(no_spawn_dungeon.find_spawn())
+
+    def test_find_instance_letter(self):
+        self.assertEqual(self.test_dungeon.find_letter(self.t_hero), 'H')
+        self.assertEqual(self.test_dungeon.find_letter(self.t_orc), 'O')
 
     def test_spawn(self):
-    
-        self.assertTrue(self.test_dungeon.spawn('player_1', self.sho_hero))
-        self.assertEqual([(4, 9)], self.test_dungeon.find_spawn())
-        self.assertTrue(self.test_dungeon.spawn('player_2', self.shatterer_orc))
+        self.assertTrue(self.test_dungeon.spawn('player_1', self.t_hero))
+        self.assertEqual(self.test_dungeon.players['player_1']['details'], self.t_hero)
+        self.assertEqual(self.test_dungeon.players['player_1']['coordinates'], (0, 0))
+
+        self.assertTrue(self.test_dungeon.spawn('player_2', self.t_orc))
+        self.assertEqual(self.test_dungeon.players['player_2']['coordinates'], (4, 9))
+        self.assertFalse(self.test_dungeon.spawn('player_1', self.t_orc))
+        self.test_dungeon.d_map = {(0, 0): '.', (0, 1): '#'}
         self.assertFalse(self.test_dungeon.find_spawn())
+        self.assertFalse(self.test_dungeon.spawn('player2', self.t_orc))
 
-    def test_get_next_position(self):
-        self.assertEqual(self.test_dungeon.get_next_position([0, 0], 'right'), [0, 1])
+    # def test_printing(self):
+        # self.test_dungeon.print_map()
+        # self.test_dungeon.spawn('Player_1', self.t_hero)
+        # self.test_dungeon.print_map()
+        # self.test_dungeon.spawn('Player_2', self.t_orc)
+        # self.test_dungeon.print_map()
+        # self.assertFalse(self.test_dungeon.spawn('Player_3', self.t_hero))
 
-    def test_move_out_of_board(self):
-        self.test_dungeon.players['player_1'] = [self.sho_hero, [0, 0]]
-        self.assertFalse(self.test_dungeon.move('player_1', 'left'))
-
-    def test_check_for_obstacle(self):
-        self.assertFalse(self.test_dungeon.check_for_obstacle([0, 0]))
-        self.assertTrue(self.test_dungeon.check_for_obstacle([1, 0]))
-        
-    def test_move(self):
-        self.test_dungeon.players['player_1'] = [self.sho_hero, [0, 0]]
-        self.assertFalse(self.test_dungeon.move('player_1', 'left'))
-        self.test_dungeon.move('player_1', 'right')
-        self.assertEqual(self.test_dungeon.dungeon[0][1], 'H')
-        self.assertEqual(self.test_dungeon.dungeon[0][0], '.')
-
-        self.assertEqual(self.test_dungeon.players['player_1'][1], [0, 1])
-        self.test_dungeon.move('player_1', 'down')
-        self.assertEqual(self.test_dungeon.players['player_1'][1], [1, 1])
-        self.assertFalse(self.test_dungeon.move('player_1', 'right'))
-
-    def test_set_weapons(self):
-        for weapon in self.test_dungeon.weapons:
-            self.assertIn(self.test_dungeon.weapons[weapon][0].type, ['SmallAxe', 'BigAxe'])
+    # def test_find_possible_locations(self):
+    #     print(self.test_dungeon.sp_weapons())
 
     def test_spawn_weapons(self):
-        test = [0, 4]
+        test = (0, 4)
         result = []
         
-        for i in range(100):
+        for i in range(200):
             dungeon = copy.deepcopy(self.test_dungeon)
             dungeon.spawn_weapons()
-            result.append(dungeon.weapons['BigAxe'][1])
-
+            result.append(dungeon.weapons['BigAxe']['coordinates'])
         self.assertIn(test, result)
 
-    def test_check_for_weapon(self):
-        self.test_dungeon.dungeon = [['H', 'W', '.'], ['.', '.', '.']]
-        self.assertTrue(self.test_dungeon.check_for_weapon([0, 1]))
-        self.assertFalse(self.test_dungeon.check_for_weapon([0, 2]))
+    def test_get_next_coordinates(self):
+        self.assertEqual(self.small.get_next_coordinates((0, 0), 'down'), (1, 0))
+        self.assertEqual(self.small.get_next_coordinates((0, 0), 'right'), (0, 1))
+        self.assertFalse(self.small.get_next_coordinates((0, 0), 'up'))
+        self.assertFalse(self.small.get_next_coordinates((0, 0), 'left'))
 
-    def test_check_for_player(self):
-        self.test_dungeon.dungeon = [['H', 'W', '.'], ['O', '.', '.']]
-        self.assertTrue(self.test_dungeon.check_for_player([1, 0]))
-        self.assertFalse(self.test_dungeon.check_for_player([0, 2]))
+    def test_get_weapon_with_coordinates(self):
+        self.small.weapons['Axe']['coordinates'] = (0, 2)
+        self.small.weapons['Sword']['coordinates'] = (2, 4)
+        self.assertEqual(self.small.get_weapon((0, 2)).type, 'Axe')
+        self.assertNotEqual(self.small.get_weapon((2, 4)).type, 'Axe')
 
-    def test_get_weapon_data_by_coordinates(self):
-        self.test_dungeon.weapons['SmallAxe'][1] = [1, 0]
-        self.assertEqual(self.test_dungeon.get_weapon([1, 0]), self.test_dungeon.weapons['SmallAxe'][0])
-    
+    def test_return_enemy(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 0)}, 'player_2': {'details': self.t_orc, 'coordinates': (0, 7)}}
+        self.assertEqual(self.small.return_enemy((0, 7)), self.t_orc)
+        self.assertNotEqual(self.small.return_enemy((0, 7)), self.t_hero)
+
+    def test_return_self(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 0)}, 'player_2': {'details': self.t_orc, 'coordinates': (0, 7)}}
+        self.assertEqual(self.small.return_self('player_2'), self.t_orc)
+
+    def test_move_outside_map(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 0)}}
+        self.assertFalse(self.small.move('player_1', 'left'))
+
+    def test_move_obstacle(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 0)}}
+        self.assertFalse(self.small.move('player_1', 'down'))
+
+    def test_move_path(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 0)}, 'player_2': {'details': self.t_orc, 'coordinates': (0, 7)}}
+        
+        self.assertTrue(self.small.move('player_1', 'right'))
+        self.assertEqual(self.small.d_map[(0, 0)], '.')
+        self.assertEqual(self.small.d_map[(0, 1)], 'H')
+
+    def test_move_weapon_case(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 1)}}
+        self.small.weapons['Axe']['coordinates'] = (0, 2)
+ 
+        self.small.d_map[(0, 2)] = 'W'
+
+        self.assertTrue(self.small.move('player_1', 'right'))
+        self.assertTrue(self.small.players['player_1']['details'].has_weapon())
+        self.assertEqual(self.small.players['player_1']['details'].weapon, self.small.weapons['Axe']['details'])
+        self.assertEqual(self.small.d_map[(0, 2)], 'H')
+        self.assertEqual(self.small.d_map[(0, 1)], '.')
+
+    def test_move_weapon_when_already_have_one(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (0, 2)}}
+        self.small.players['player_1']['details'].weapon = self.small.weapons['Axe']['details']
+        self.small.weapons['Sword']['coordinates'] = (1, 2)
+
+        self.small.d_map[(1, 2)] = 'W'
+
+        self.assertTrue(self.small.players['player_1']['details'].has_weapon())
+        self.assertEqual(self.small.players['player_1']['details'].weapon, self.small.weapons['Axe']['details'])
+        self.assertEqual(self.small.d_map[(0, 2)], '.')
+        self.assertEqual(self.small.d_map[(0, 1)], '.')
+
+        self.assertTrue(self.small.move('player_1', 'down'))
+        self.assertEqual(self.small.players['player_1']['details'].weapon, self.small.weapons['Sword']['details'])
+        self.assertEqual(self.small.d_map[(1, 2)], 'H')
+        self.assertEqual(self.small.d_map[(0, 2)], '.')
+
+    def test_move_enemy_case(self):
+        self.small.players = {'player_1': {'details': self.t_hero, 'coordinates': (1, 2)}, 'player_2': {'details': self.t_orc, 'coordinates': (1, 3)}}
+        self.small.players['player_1']['details'].weapon = self.small.weapons['Axe']['details']
+        self.small.players['player_2']['details'].weapon = self.small.weapons['Axe']['details']
+
+        result = []
+        for winner in range(100):
+            self.small.players['player_1']['details'].health = 100
+            self.small.players['player_2']['details'].health = 100
+            result.append(self.small.enemy_case((1, 3), 'player_1'))
+
+        self.assertIn('Shosh', result)
+        self.assertIn('Shatterer', result)
+
+        
+        
+        
+        
+        
+
+
+        
 if __name__ == '__main__':
     unittest.main()
